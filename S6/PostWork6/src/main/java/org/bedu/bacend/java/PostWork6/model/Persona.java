4b4eaf6313ec4b751dd37c5da891213fa51b8c11
Alex Hernandez Duran <hernandezduranluisalejandro4@gmail.com>
2023-03-16 15:18:08 -0600

    Se agrega PostWork de la Sessión 6

---
+++ S6/PostWork6/src/main/java/org/bedu/bacend/java/PostWork6/model/Persona.java
@@ -0,0 +1,67 @@
+package org.bedu.bacend.java.PostWork6.model;
+
+import java.util.Objects;
+
+public class Persona implements Comparable<Persona> {
+    private String nombre;
+    private String telefono;
+
+    public Persona() {
+    }
+
+    public Persona(String nombre, String telefono) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", telefono='" + telefono + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Persona persona = (Persona) o;
+        return nombre.equals(persona.nombre);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombre);
+    }
+
+    @Override
+    public int compareTo(Persona o) {
+        return this.nombre.compareTo(o.nombre);
+    }
+}
+
+//La clase tiene dos variables miembro privadas: nombre y telefono, ambas de tipo String.
+//La clase tiene dos constructores: uno sin argumentos y otro que toma un nombre y un telefono.
+// También hay métodos get y set para acceder y modificar las variables miembro nombre y telefono.
+//La clase tiene tres métodos anulados (override) de la clase Object. El método toString() devuelve una cadena
+// que representa el objeto Persona, el método equals()
+// se utiliza para comparar dos objetos Persona y el método hashCode() devuelve un código hash del objeto.
+//La clase también implementa la interfaz Comparable, lo que significa que los objetos Persona pueden ser
+// comparados y ordenados entre sí. La comparación se realiza en función del valor de nombre de cada objeto.
---
+++ S6/PostWork6/src/main/java/org/bedu/bacend/java/PostWork6/persistence/AgendaMemoryDao.java
@@ -0,0 +1,38 @@
+package org.bedu.bacend.java.PostWork6.persistence;
+
+import org.bedu.bacend.java.PostWork6.model.Persona;
+import org.springframework.stereotype.Repository;
+
+import java.util.Set;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+@Repository
+public class AgendaMemoryDao {
+
+    private static final SortedSet<Persona> personas = new TreeSet<>();
+
+    public Persona guardaPersona(Persona persona) {
+        personas.add(persona);
+        return persona;
+    }
+
+    public Set<Persona> getPersonas() {
+        return personas;
+    }
+}
+
+//La clase tiene una variable privada llamada personas, que es una instancia de la clase SortedSet<Persona>.
+// Un SortedSet es un conjunto que mantiene los elementos ordenados en función de un comparador proporcionado.
+// En este caso, la clase Persona debe implementar la interfaz Comparable<Persona> para definir un orden natural
+// para las instancias de Persona.
+//La clase tiene dos métodos públicos llamados guardaPersona y getPersonas. El método guardaPersona acepta
+// una instancia de Persona como entrada y devuelve una instancia de Persona como salida. Este método agrega
+// la instancia de Persona proporcionada al conjunto personas utilizando el método add y luego devuelve la misma
+// instancia.
+//El método getPersonas devuelve un conjunto de instancias de Persona. Este método devuelve el conjunto personas
+// sin modificarlo.
+//En resumen, esta clase es una implementación de un DAO (Data Access Object) que proporciona métodos
+// para almacenar y recuperar instancias de Persona en memoria. Utiliza un SortedSet para mantener las
+// instancias de Persona en un orden natural y proporciona métodos para agregar y recuperar instancias de Persona
+// del conjunto.
